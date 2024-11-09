200a5fa4673ef7cc6fe38c02f41a4f3ece481c00
Veronika06278 <veronika.zheleva2006@gmail.com>
2024-11-09 12:07:22 +0200

    Initial commit

---
+++ SuperVet/SuperVet.Data/SuperVetDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperVet.Data
+{
+    public class SuperVetDbContext : IdentityDbContext
+    {
+        public SuperVetDbContext(DbContextOptions<SuperVetDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
